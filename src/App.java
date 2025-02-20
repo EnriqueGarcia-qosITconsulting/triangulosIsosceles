3338cd8b6004e1e16d41a14e75e0f50e72bf1982
Egararj <egararj@gmail.com>
2025-02-20 13:29:01 +0100

    optimizado

--- src/App.java
+++ src/App.java
@@ -26,24 +26,21 @@ public class App {
         int largo = Integer.parseInt(triangulo[0]);
         String letra = triangulo[1];
         String lineaTriangulo = "";
-        for (int x=1, cont=0; x<=largo; x++,cont++){
+        for (int x=1; x<=largo; x++){
             lineaTriangulo = "";
-            if (x < largo){
-                for (int y=1; y<largo-x+1; y++){//inserta los sitios en blanco a la izquierda
-                    lineaTriangulo += "-";
-                }
-                for (int y=1; y<=x+cont; y++){//inserta las letras
-                    lineaTriangulo += letra;
-                }
-                for (int y=1; y<largo-x+1; y++){//inserta los sitios en blanco a la derecha
-                    lineaTriangulo += "-";
-                }
+
+            for (int y=1; y<largo-x+1; y++){//inserta los sitios en blanco a la izquierda
+                lineaTriangulo += "-";
             }
-            else{
-                for (int y=0; y<largo+cont; y++){//inserta la última línea
-                    lineaTriangulo += letra;
-                }
+
+            for (int y=1; y<=x+(x-1); y++){//inserta las letras
+                lineaTriangulo += letra;
             }
+
+            for (int y=1; y<largo-x+1; y++){//inserta los sitios en blanco a la derecha
+                lineaTriangulo += "-";
+            } 
+
             System.out.println(lineaTriangulo);
         }
     }
